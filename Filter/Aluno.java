69a88d66b4691bbf0dcc292532a6bcebcddc94be
Guilherme <guilhermepolaski@protonmail.com>
2025-09-22 17:28:07 -0300

    MinMax e Match

--- Filter/Aluno.java
+++ Filter/Aluno.java
@@ -1,5 +1,7 @@
 package Streams.Filter;
 
+import java.util.Objects;
+
 public class Aluno {
 
     final String nome;
@@ -8,5 +10,20 @@ public class Aluno {
     public Aluno(String nome, double nota) {
         this.nome = nome;
         this.nota = nota;
+    }
+
+    public String toString() {
+        return nome + " tem nota " + nota;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Aluno aluno)) return false;
+        return Double.compare(nota, aluno.nota) == 0 && Objects.equals(nome, aluno.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, nota);
     }
 }
